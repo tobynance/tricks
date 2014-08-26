659ee293b9b1788bebe7a31b2d71ab3b630f0c53
Toby Nance <toby.nance@gmail.com>
2014-08-26 18:25:22 -0500

    Moved to only having the Python client enabled by default

--- src/tricks/core.clj
+++ src/tricks/core.clj
@@ -19,8 +19,8 @@
 (defn -main
   [& args]
   (tricks.server/run [
-                      ["Clojure1" ["java" "-jar" "clojure_client-0.1.0-SNAPSHOT-standalone.jar" "Clojure1"] (java.io.File. "clients")]
-                      ;["Python_1" ["python" "simple_client.py" "Python_1"] (java.io.File. "clients")]
+                      ;["Clojure1" ["java" "-jar" "clojure_client-0.1.0-SNAPSHOT-standalone.jar" "Clojure1"] (java.io.File. "clients")]
+                      ["Python_1" ["python" "simple_client.py" "Python_1"] (java.io.File. "clients")]
                       ["Python_4" ["python" "simple_client.py" "Python_4"] (java.io.File. "clients")]
                       ["Python_2" ["python" "simple_client.py" "Python_2"] (java.io.File. "clients")]
                       ["Python_3" ["python" "simple_client.py" "Python_3"] (java.io.File. "clients")]]))
